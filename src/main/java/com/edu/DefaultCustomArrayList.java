c2c3cc60abc0b0f734a60e7b2c7fd956acda19d1
Pavelsphone <64527633+Pavelsphone@users.noreply.github.com>
2024-10-09 12:35:40 +0500

    Update DefaultCustomArrayList.java

--- src/main/java/com/edu/DefaultCustomArrayList.java
+++ src/main/java/com/edu/DefaultCustomArrayList.java
@@ -5,8 +5,19 @@ import java.util.Iterator;
 public class DefaultCustomArrayList<E> implements CustomArrayList<E> {
 
     public DefaultCustomArrayList() {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
     }
-
+    
     @Override
     public boolean add(E element) {
         return true;
